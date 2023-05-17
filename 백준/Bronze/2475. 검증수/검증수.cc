7ddf480dd744c31d7552deec9b20e0031c3650e7
박윤형 <62279820+pyh4456@users.noreply.github.com>
2023-05-17 17:16:33 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	int arr[5];
+
+	cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+	cout << (arr[0] * arr[0] + arr[1] * arr[1] + arr[2] * arr[2] + arr[3] * arr[3] + arr[4] * arr[4]) % 10;
+}
