b8b7a0b8cd17a6cb23946a802388952f8a2be152
박윤형 <62279820+pyh4456@users.noreply.github.com>
2023-05-18 16:08:06 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+int parent[1000001] = { 0 };
+int value[1000001] = { 0 };
+
+void memory(int n) {
+	int temp;
+
+	for (int i = 2; i <= n; i++) {
+		value[i] = value[i - 1] + 1;
+		parent[i] = i - 1;
+
+		if (i % 3 == 0) {
+			temp = value[i / 3] + 1;
+			if (value[i] > temp) {
+				value[i] = temp;
+				parent[i] = i / 3;
+ 			}
+		}
+		if (i % 2 == 0) {
+			temp = value[i / 2] + 1;
+			if (value[i] > temp) {
+				value[i] = temp;
+				parent[i] = i / 2;
+			}
+		}
+	}
+}
+
+void print_sequense(int n) {
+	while (n != 1) {
+		printf("%d ", n);
+		n = parent[n];
+	}
+	cout << 1;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	
+	memory(n);
+
+	cout << value[n] << endl;
+	print_sequense(n);
+}
