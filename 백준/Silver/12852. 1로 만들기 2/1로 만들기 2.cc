4903e6318605de066d0d38175cb6775cf0b3390e
박윤형 <62279820+pyh4456@users.noreply.github.com>
2023-05-18 16:17:21 +0900

    Update 1로 만들기 2.cc
    주석추가

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -5,17 +5,19 @@
 
 using namespace std;
 
-int parent[1000001] = { 0 };
-int value[1000001] = { 0 };
+int parent[1000001] = { 0 };	// n을 1로 만들기위한 과정의 첫단계의 수를 나타낸다.
+int value[1000001] = { 0 };	// value[n] : n을 1로 만들기 위한 횟수
 
-void memory(int n) {
+void memory(int n) {	//2부터 n까지의 모든 수에 대해, 1로 만들기 위한 횟수를 value에 저장.
 	int temp;
 
 	for (int i = 2; i <= n; i++) {
+		// n에서 1을 빼본다.
 		value[i] = value[i - 1] + 1;
 		parent[i] = i - 1;
 
 		if (i % 3 == 0) {
+			//n을 3으로 나눠본다.
 			temp = value[i / 3] + 1;
 			if (value[i] > temp) {
 				value[i] = temp;
@@ -23,6 +25,7 @@ void memory(int n) {
  			}
 		}
 		if (i % 2 == 0) {
+			//n을 2로 나눠본다.
 			temp = value[i / 2] + 1;
 			if (value[i] > temp) {
 				value[i] = temp;
@@ -32,7 +35,7 @@ void memory(int n) {
 	}
 }
 
-void print_sequense(int n) {
+void print_sequense(int n) {	//n을 1로 만들기위한 과정을 보여준다.
 	while (n != 1) {
 		printf("%d ", n);
 		n = parent[n];
